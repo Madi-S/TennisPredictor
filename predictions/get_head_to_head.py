a7b951c971930de4fdb8ccc34c276cf5e1f5f4ba
Madi <72963376+Madi-S@users.noreply.github.com>
2020-12-30 19:28:19 +0600

    test scrip to get h2h

--- predictions/get_head_to_head.py
+++ predictions/get_head_to_head.py
@@ -7,28 +7,20 @@ from bs4 import BeautifulSoup
 from webdriver import Webdriver
 
 
-HTH = 'https://www.tennisexplorer.com/'
-OUTCOME = 'https://www.tennisprediction.com/'
+URL = 'https://www.tennisexplorer.com/'
+
 
 # Selectors:
 MATCHES_SELECTOR = '.inner.noPgA'
-NAV_SELECTOR = '#navlist li'
 
 
 
-class TennisComparsion(Webdriver):
+class TennisExplorer(Webdriver):
 
     async def get_head_to_head(self, player: str):
-        await self._page._goto_retry(HTH, MATCHES_SELECTOR)
+        await self._page._goto_retry(URL, MATCHES_SELECTOR)
         sleep(4)
         await self._page.waitForNavigation()
 
         html = await self._page.content()
 
-
-    async def get_outcome(self, players: list):
-        await self._page._goto_retry(OUTCOME, NAV_SELECTOR)
-        sleep(4)
-        await self._page.waitForNavigation()
-
-        await 
