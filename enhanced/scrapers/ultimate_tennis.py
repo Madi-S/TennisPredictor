b6961267ef3f230d97fa325dd60629dda81c5862
Madi <72963376+Madi-S@users.noreply.github.com>
2021-01-03 19:43:20 +0600

    firefox webdriver to render js added

--- enhanced/scrapers/ultimate_tennis.py
+++ enhanced/scrapers/ultimate_tennis.py
@@ -3,6 +3,7 @@ import re
 
 from datetime import datetime
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
 URL = 'https://www.ultimatetennisstatistics.com'
@@ -25,8 +26,7 @@ def get_player_id(name):
 
     return found[0]['id']
 
-
-def parse_profiles(html, players, surface):
+def parse_html(html, players, surface):
     soup = BeautifulSoup(html, 'lxml')
     table = soup.select_one('.table.table-condensed.text-nowrap')
 
@@ -40,62 +40,82 @@ def parse_profiles(html, players, surface):
         order.append(surface)
 
     for i in range(2):
+        p = players[i]
+        data[p] = {}
         if i == 1:
             base = lambda o: table.find(text=re.compile(fr'{o}')).parent
-            find_h2h = lambda o : base(o).next_sibling.next_sibling.a.text.strip()
-            find_surface = lambda o: table.find(text=re.compile(fr'{o}'), class_='text-center').next_sibling.next_sibling.text.strip()
-            find_some = lambda o : base(o).parent.next_sibling.next_sibling.text.strip()
-            find_any = lambda o: base(o).next_sibling.next_sibling.text.strip()
+            find_h2h = lambda o : base(o).next_sibling.next_sibling.a
+            find_surface = lambda o: table.find(text=re.compile(fr'{o}'), class_='text-center').next_sibling.next_sibling
+            find_some = lambda o : base(o).parent.next_sibling.next_sibling
+            find_any = lambda o: base(o).next_sibling.next_sibling
         else:
             base = lambda o: table.find(text=re.compile(fr'{o}')).parent
-            find_h2h = lambda o : base(o).previous_sibling.previous_sibling.a.text.strip()
-            find_surface = lambda o: table.find(text=re.compile(fr'{o}'), class_='text-center').previous_sibling.previous_sibling.text.strip()
-            find_some = lambda o : base(o).parent.previous_sibling.previous_sibling.text.strip()
-            find_any = lambda o: base(o).previous_sibling.previous_sibling.text.strip()
+            find_h2h = lambda o : base(o).previous_sibling.previous_sibling.a
+            find_surface = lambda o: table.find(text=re.compile(fr'{o}'), class_='text-center').previous_sibling.previous_sibling
+            find_some = lambda o : base(o).parent.previous_sibling.previous_sibling
+            find_any = lambda o: base(o).previous_sibling.previous_sibling
             
         for o in order:
-            if 'H2H' in o:
-                info = find_h2h(o) 
-            elif o == 'Best Season' or o == 'Overall':
-                info = find_some(o)
-            elif o in ['Hard','Grass','Clay']:
-                info = find_surface(o)
-            else:
-                info = find_any(o)
-            data[players[i]][o] = info
+            print(o)
+            try:
+                if 'H2H' in o:
+                    info = find_h2h(o) 
+                elif o == 'Best Season' or o == 'Overall':
+                    info = find_some(o)
+                elif o in ['Hard','Grass','Clay']:
+                    info = find_surface(o)
+                else:
+                    info = find_any(o)
+                data[p][o] = info.text.strip()
+            except:
+                data[p][o] = None
 
-    return data
+    order = ['Ace %', 'Double Fault %', '1st Serve %','1st Serve Won %', '2nd Serve Won %', 
+    'Break Points Saved %', 'Service Points Won %',
+    'Points Dominance', 'Games Dominance', 'Return Points Won %']
 
-
-def parse_stats(html):
-    soup = BeautifulSoup(html, 'lxml')
-    data = []
+    for i in range(0, 2):
+        base = lambda d: soup.find(text=re.compile(fr'{d}'), class_='text-center')
+        if i == 1:
+            print('Player 2:')
+            find = lambda d: base(d).parent.select_one('.text-left')
+        else:
+            print('Player 1:')
+            find = lambda d: base(d).parent.select_one('.text-right')
+        for d in data:
+            data[p][o] = find(d).text.strip()
 
     return data
 
 
     
-def compare_players(p1, p2, tournament):
+def compare_players(p1, p2, tournament=None, surface=None):
     id1 = get_player_id(p1)
     id2 = get_player_id(p2)
 
     if not (id1 and id2):
         return None
 
-    url = 'https://www.ultimatetennisstatistics.com/headToHead?playerId1={id1}&playerId2={id2}'
-    r = requests.get(url.format(id1, id2), headers=headers)
+    url = 'https://www.ultimatetennisstatistics.com/headToHead?playerId1={}&playerId2={}'
+    driver = webdriver.Firefox()
+
+    with open('hau.html','w') as f:
+        f.write(driver.page_source)
+        print('written')
     
 
     if not r.ok:
         raise AttributeError(f'Bad response from UltimateTennis: {r}. Fix the issue')
 
-    html = r.text
-    profiles = parse_profiles(html, [p1, p2], surface)
-    profiles[p1]['past_matches'] = get_past_matches(id1) 
-    profiles[p2]['past_matches'] = get_past_matches(id2) 
+    profiles = parse_html(r.text, [p1, p2], surface)
+    with open('profiles.txt','w') as f:
+        f.write(str(profiles))
+    # stats[p1]['past_matches'] = get_past_matches(id1) 
+    # stats[p2]['past_matches'] = get_past_matches(id2) 
+
 
 def get_past_matches(id_):
-    pass
+    return 1
 
 
 '''
@@ -156,4 +176,4 @@ Response:
 '''
 
 if __name__ == '__main__':
-    print(find_player('David G'))
+    compare_players('Paire','Goffin',surface='Grass')
