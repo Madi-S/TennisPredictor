bcf308e8df091809333ede6c682311ec98a12782
Madi <72963376+Madi-S@users.noreply.github.com>
2021-01-04 12:41:10 +0600

    test files delted headers compiled

--- enhanced/scrapers/ultimate_tennis.py
+++ enhanced/scrapers/ultimate_tennis.py
@@ -111,12 +111,11 @@ def compare_players(p1, p2, tournament=None, surface=None):
         f.write(html)
         print('written')
 
-    profiles = parse_html(html, [p1, p2], surface)
-    with open('profiles.txt','w') as f:
-        f.write(str(profiles))
-    # stats[p1]['past_matches'] = get_past_matches(id1) 
-    # stats[p2]['past_matches'] = get_past_matches(id2) 
+    stats = parse_html(html, [p1, p2], surface)
+    stats[p1]['past_matches'] = get_past_matches(id1) 
+    stats[p2]['past_matches'] = get_past_matches(id2) 
 
+    return stats
 
 
 
@@ -125,33 +124,33 @@ def get_past_matches(id_):
 
 
 '''
-Request URL: https://www.ultimatetennisstatistics.com/matchesTable?playerId=5324&current=1&rowCount=15&sort%5Bdate%5D=desc&searchPhrase=&season=&fromDate=&toDate=&level=&bestOf=&surface=&indoor=&speed=&round=&result=&opponent=&tournamentId=&tournamentEventId=&outcome=&score=&countryId=&bigWin=false&_=1609678509654
+--GENERAL---
+Request URL:
+https://www.ultimatetennisstatistics.com/
+matchesTable?playerId=5663&current=1&rowCount=15&sort%5Bdate%5D=desc&searchPhrase=&season=&
+fromDate=&toDate=&level=&bestOf=&surface=&indoor=&speed=&round=&result=&opponent=&tournamentId=&
+tournamentEventId=&outcome=&score=&countryId=&bigWin=false&_=1609742224223
+
 Request Method: GET
 Status Code: 200 
 Remote Address: 46.101.166.59:443
 Referrer Policy: strict-origin-when-cross-origin
-Cache-Control: private
-Connection: keep-alive
-Content-Encoding: gzip
-Content-Type: application/json
-Date: Sun, 03 Jan 2021 12:55:14 GMT
-Expires: Thu, 01 Jan 1970 00:00:00 GMT
-Keep-Alive: timeout=60
-Transfer-Encoding: chunked
-vary: accept-encoding
+'''
+'''
+---HEADERS---
 Accept: */*
 Accept-Encoding: gzip, deflate, br
 Accept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7
 Connection: keep-alive
 Cookie: _ga=GA1.2.659216525.1609145839; _gid=GA1.2.1340506244.1609145839; __gads=ID=a110e92986bf849e-225e69d973b9003b:T=1609145842:RT=1609145842:S=ALNI_Ma4Yl3UW1qQ2GVAeX1FXzaLBttlhg; _gat=1
 Host: www.ultimatetennisstatistics.com
-Referer: https://www.ultimatetennisstatistics.com/playerProfile?playerId=5324
+Referer: https://www.ultimatetennisstatistics.com/playerProfile?playerId=5663
 Sec-Fetch-Dest: empty
 Sec-Fetch-Mode: cors
 Sec-Fetch-Site: same-origin
 User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36
 X-Requested-With: XMLHttpRequest
-playerId: 5324
+playerId: 5663
 current: 1
 rowCount: 15
 sort[date]: desc
@@ -173,13 +172,8 @@ outcome:
 score: 
 countryId: 
 bigWin: false
-_: 1609678509654
-'''
-
-'''
-Response:
-[{"id":"4742","value":"Rafael Nadal","label":"Rafael Nadal (ESP)"},{"id":"3970","value":"David Ferrer","label":"David Ferrer (ESP)"},{"id":"3786","value":"Nikolay Davydenko","label":"Nikolay Davydenko (RUS)"},{"id":"3900","value":"David Nalbandian","label":"David Nalbandian (ARG)"},{"id":"1434","value":"Petr Korda","label":"Petr Korda (CZE)"},{"id":"6407","value":"Daniil Medvedev","label":"Daniil Medvedev (RUS)"},{"id":"55","value":"Cliff Drysdale","label":"Cliff Drysdale (RSA)"},{"id":"5663","value":"David Goffin","label":"David Goffin (BEL)"},{"id":"4269","value":"Fernando Verdasco","label":"Fernando Verdasco (ESP)"},{"id":"1649","value":"Guillermo Perez Roldan","label":"Guillermo Perez Roldan (ARG)"},{"id":"4570","value":"Marcos Baghdatis","label":"Marcos Baghdatis (CYP)"},{"id":"1609","value":"David Wheaton","label":"David Wheaton (USA)"},{"id":"855","value":"Scott Davis","label":"Scott Davis (USA)"},{"id":"946","value":"Slobodan Zivojinovic","label":"Slobodan Zivojinovic (SRB)"},{"id":"2539","value":"Bohdan Ulihrach","label":"Bohdan Ulihrach (CZE)"},{"id":"1190","value":"Darren Cahill","label":"Darren Cahill (AUS)"},{"id":"815","value":"David Pate","label":"David Pate (USA)"},{"id":"967","value":"Dan Goldie","label":"Dan Goldie (USA)"},{"id":"1686","value":"Franco Davin","label":"Franco Davin (ARG)"},{"id":"2106","value":"Davide Sanguinetti","label":"Davide Sanguinetti (ITA)"}]
+_: 1609742224223
 '''
 
 if __name__ == '__main__':
-    compare_players('Paire','Goffin',surface='Grass')
+    compare_players('Paire', 'Goffin', surface='Grass')
